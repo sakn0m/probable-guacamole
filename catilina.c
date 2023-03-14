9b0ce88664c1532b6387c7b3184db666fc3cb892
sakn0m <81561474+sakn0m@users.noreply.github.com>
2023-03-14 16:06:56 +0100

    Update and rename leopard1.c to catilina.c

--- catilina.c
+++ catilina.c
@@ -1,15 +1,22 @@
-#include <stdio.h>
-
-int main() {
+int main() 
+{
     int anno;
+    
     printf("Inserisci il tuo anno di nascita: ");
     scanf("%d", &anno);
-    if (anno == 1969) {
+    
+    if (anno == 1969) 
+    {
         printf("Sei nato l'anno in cui l'uomo è andato sulla Luna!\n");
-    } else if (anno < 1969) {
+    } 
+    else if (anno < 1969) 
+    {
         printf("Sei nato %d anni prima che l'uomo andasse sulla Luna.\n", 1969 - anno);
-    } else {
+    } 
+    else 
+    {
         printf("Sei nato %d anni dopo che l'uomo è andato sulla Luna.\n", anno - 1969);
     }
     return 0;
 }
+
