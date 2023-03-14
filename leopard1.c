dbb04953d16476dda47aeb75d56e3bc42ec26ab1
sakn0m <81561474+sakn0m@users.noreply.github.com>
2023-03-14 16:02:03 +0100

    Create leopard1.c

---
+++ leopard1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int anno;
+    printf("Inserisci il tuo anno di nascita: ");
+    scanf("%d", &anno);
+    if (anno == 1969) {
+        printf("Sei nato l'anno in cui l'uomo è andato sulla Luna!\n");
+    } else if (anno < 1969) {
+        printf("Sei nato %d anni prima che l'uomo andasse sulla Luna.\n", 1969 - anno);
+    } else {
+        printf("Sei nato %d anni dopo che l'uomo è andato sulla Luna.\n", anno - 1969);
+    }
+    return 0;
+}
