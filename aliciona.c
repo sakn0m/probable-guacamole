a3cc038fd4e6fb426f75d69fcba54f317312529d
sakn0m <81561474+sakn0m@users.noreply.github.com>
2023-03-15 10:12:22 +0100

    Update aliciona.c

--- aliciona.c
+++ aliciona.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    int i = 2;
-    int partial_sum = 0;
-    while (i <= 100) {
-        partial_sum += i;
-        printf("Somma parziale: %d\n", partial_sum);
-        i += 2;
+int main() 
+{
+    int x;
+    int y;
+    x = 0;
+    y = 0;
+
+    while (x<=100)
+    {   
+        x=x+2;
+        y=y+x;
     }
-    printf("La somma finale dei numeri pari compresi tra 2 e 100 è: %d\n", partial_sum);
-    return 0;
+    printf("la somma è: %d\n", y);
+  
 }
